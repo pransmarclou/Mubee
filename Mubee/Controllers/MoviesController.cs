b413ae192e80a898caaf749d84a6ee9253e49caa
prans <fhranzalimorong@gmail.com>
2017-12-30 22:30:13 +0800

    Added Customers and Movies Page

--- Mubee/Controllers/MoviesController.cs
+++ Mubee/Controllers/MoviesController.cs
@@ -10,11 +10,18 @@ namespace Mubee.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
         //GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Taken"};
-          
+            var movie = new Movie() { Name = "Taken" };
+
             //return Content("Hello World!");
             //return HttpNotFound();
             //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
@@ -22,7 +29,7 @@ namespace Mubee.Controllers
             //ViewData["Movie"] = movie;
             //ViewBag.Movie = movie;
 
-            
+
             // var viewResult = new ViewResult();
             //viewResult.ViewData.Model
             var customers = new List<Customer>
@@ -44,15 +51,15 @@ namespace Mubee.Controllers
             return Content("id=" + id);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
 
-            return Content(String.Format("pageIndex{0}&sortBy={1}",pageIndex,sortBy));
-        }
+        //    return Content(String.Format("pageIndex{0}&sortBy={1}", pageIndex, sortBy));
+        //}
 
         //Regex for regular expression
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
@@ -61,5 +68,13 @@ namespace Mubee.Controllers
             return Content(year + "/" + month);
         }
 
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id=1, Name = "Shrek" },
+                new Movie {Id=2, Name ="Wall-e"}
+            };
+        }
     }
 }
